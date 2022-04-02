a0bb497329ff655a32fcc71aaf892bd9ec80f102
Victor Jiménez <vcjimenez1@espe.edu.ec>
2022-04-02 17:53:36 -0500

    UpdateCheckout

--- src/app.ts
+++ src/app.ts
@@ -2,8 +2,6 @@ import express, { Express, Response } from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
 
-import { router } from './routes/actions';
-
 dotenv.config();
 
 const app: Express = express();
@@ -12,7 +10,6 @@ const port: string = process.env.PORT || "4000";
 app.use(cors());
 app.use(express.json());
 
-app.use('/actions', router);
-app.use((res: Response) => res.status(404).json({msg: '404 not found'}));
+// TODO: Routing, Middlewares, Apis...
 
 app.listen((port as string), () => console.log('Server running at', port));
